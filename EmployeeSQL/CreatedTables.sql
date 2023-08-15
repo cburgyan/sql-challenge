884c19b1ae2effdfcdca3316308754e544356581
cburgyan <cburgyan@gmail.com>
2023-08-15 17:57:45 -0500

    rough completion of creating tables and queries .sql files

---
+++ EmployeeSQL/CreatedTables.sql
@@ -0,0 +1,41 @@
+CREATE TABLE "departments" (
+    "dept_no" VARCHAR(4) PRIMARY KEY,
+    "dept_name" VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE "titles" (
+    "title_id" VARCHAR(5) PRIMARY KEY,
+    "title" VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE "employees" (
+    "emp_no" INT PRIMARY KEY,
+    "emp_title_id" VARCHAR(5) NOT NULL,
+    "birth_date" DATE NULL,
+    "first_name" VARCHAR(50) NOT NULL,
+    "last_name" VARCHAR(50) NOT NULL,
+    "sex" VARCHAR(1) NULL,
+    "hire_date" DATE NOT NULL
+);
+
+CREATE TABLE "salaries" (
+    "emp_no" INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+    "salary" FLOAT NOT NULL
+);
+
+CREATE TABLE "dept_emp" (
+    "emp_no" INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+    "dept_no" VARCHAR(4) NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	PRIMARY KEY (emp_no, dept_no)
+);
+
+CREATE TABLE "dept_manager" (
+    "dept_no" VARCHAR(4) NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+    "emp_no" INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	PRIMARY KEY (emp_no, dept_no)
+);
